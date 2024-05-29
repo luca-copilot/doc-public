4eca20dfae025bd88530ddca854e3ce02f3920e9
Vitor Costa <vitormc@gmail.com>
2024-05-29 17:45:00 -0300

    First commit

---
+++ assets/js/de8b21c0.d6a733f0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocumentation=self.webpackChunkdocumentation||[]).push([[438],{8406:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/doc-public/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
